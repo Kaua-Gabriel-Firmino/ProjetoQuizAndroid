74d359737a76425a7879c8ca922c6134cfdb14e9
kaua-gabriel-firmino <kauafirmino9980@gmail.com>
2025-09-18 09:33:51 -0300

    adicionado ultima parte

--- app/src/main/java/com/example/quizes/MainActivity.java
+++ app/src/main/java/com/example/quizes/MainActivity.java
@@ -42,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        txvPergunta = findViewById(R.id.txvPergunta);
+        rdgroup = findViewById(R.id.rdgroup);
+        btnPergunta = findViewById(R.id.btnresponder);
+
+        //nosso metodo!
+        carregarPerguntas();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
